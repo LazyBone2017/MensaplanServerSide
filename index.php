890967a8367b6cde64bc3d49cfd5541789f5eff0
Birdy2014 <moritzv7@googlemail.com>
2018-05-27 22:39:38 +0200

    It works now and the Mensaplan is only updated, if it is too old.

--- index.php
+++ index.php
@@ -1,46 +1,55 @@
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-    <?php
-    include('lib/PDF2TEXT.php');
-    /**
-     * Created by PhpStorm.
-     * User: Jonas Schroeter
-     * Date: 27.05.2018
-     * Time: 13:23
-     */
-    downloadMensaplan();
-    function downloadMensaplan(){
-        $week = date("W");
-        $year = date("Y");
-        $day = date("l");
-        if($day == "Saturday" || $day == "Sunday"){
-            $week++;
-        }
-        $url = "https://www.liebigmensaservice.de/speiseplan/sppl-asbhelmholtz-" . $week . $year . ".pdf";
-
-        $a = new PDF2Text();
-        $a->setFilename($url);
-        $a->decodePDF();
-        $pdfString =  $a->output();
-
-        $pdfString = substr($pdfString, strpos($pdfString, "vegan") + 5);
-        $pdfString = substr($pdfString, 0, strpos($pdfString, "Tagen") - 11);
-        $dayArray = array();
-        $days = array("Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag");
-        for($i = 0; $i < sizeof($days); $i++) {
-            $dayArray[$i] = substr($pdfString, 0, strpos($pdfString, $days[$i]));
-            if($i == sizeof($days) - 1){
-                $dayArray[$i] = $pdfString;
-            }
-            $pdfString = substr($pdfString, strpos($pdfString, $days[$i]));
-            echo $dayArray[$i] . "<br>";
-        }
+<?php
+include('lib/PDF2TEXT.php');
+/**
+ * Created by PhpStorm.
+ * User: Jonas Schroeter
+ * Date: 27.05.2018
+ * Time: 13:23
+ */
+downloadMensaplan();
+function downloadMensaplan()
+{
+    $week = date("W");
+    $year = date("Y");
+    $day = date("l");
+    if ($day == "Saturday" || $day == "Sunday") {
+        $week++;
     }
-    ?>
-    </body>
-</html>
 
+    $fileLastUpdate = fopen("./lastUpdate.json", "r") or die("Unable to open file!");
+    $jsonLastUpdate = fread($fileLastUpdate, filesize("lastUpdate.json"));
+    if (json_decode($jsonLastUpdate)[5] == $week) {
+        echo json_decode($jsonLastUpdate);
+        fclose($fileLastUpdate);
+        return;
+    }
+    fclose($fileLastUpdate);
+
+    $url = "https://www.liebigmensaservice.de/speiseplan/sppl-asbhelmholtz-" . $week . $year . ".pdf";
+
+    $a = new PDF2Text();
+    $a->setFilename($url);
+    $a->decodePDF();
+    $pdfString = $a->output();
+    $pdfString = trim(preg_replace('/\s+/', ' ', $pdfString));
+    $pdfString = utf8_encode($pdfString);
+
+    $pdfString = substr($pdfString, strpos($pdfString, "vegan") + 5);
+    $pdfString = substr($pdfString, 0, strpos($pdfString, "Tagen") - 11);
+    $dayArray = array();
+    $days = array("Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag");
+    for ($i = 0; $i < sizeof($days); $i++) {
+        $dayArray[$i] = substr($pdfString, 0, strpos($pdfString, $days[$i]));
+        if ($i == sizeof($days) - 1) {
+            $dayArray[$i] = $pdfString;
+        }
+        $pdfString = substr($pdfString, strpos($pdfString, $days[$i]));
+        //echo $dayArray[$i] . "<br>";
+    }
+    $dayArray[5] = $week . $year;
+    $jsonString = json_encode($dayArray);
+    echo $jsonString;
+    $fileLastUpdate = fopen("./lastUpdate.json", "w+") or die("Unable to open file!");
+    fwrite($fileLastUpdate, $jsonString);
+    fclose($fileLastUpdate);
+}
